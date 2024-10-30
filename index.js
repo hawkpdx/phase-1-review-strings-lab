58b7d4fca72f50604d2cde5f0ce85a6211c33ec3
Garrett <hawkpdx@icloud.com>
2024-10-29 23:16:20 -0700

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Garrett';
+const welcomeMessage = `'Welcome to Flatbook, ' + 'Garrett'!`;
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + 'GARRETT!';
+const shortGreeting = 'Welcome, ' + 'G!';
